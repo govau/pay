9eb80d468329b0f4248693fc292d449fc656a460
Peter DeGroff <peter.degroff@gmail.com>
2019-09-18 22:49:07 +1000

    deploys: undeploy deleted branches

---
+++ ci/remove_feat_prefix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+feat_prefix = 'feat-'
+
+def featless_branch(git_output):
+    sha, remote = git_output.split()
+    branch_name = os.path.basename(remote)
+    return branch_name.replace(feat_prefix, '', 1)
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        sys.stdout.write('{branch}\n'.format(branch=featless_branch(line)))
+
+    if not line:
+        raise Exception('no branches provided')
