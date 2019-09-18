df4bbe3358fdeda30c6bed5cb86fa21cad193128
Peter DeGroff <peter.degroff@gmail.com>
2019-09-18 14:24:56 +1000

    repo: pull database from vcap in prod

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,17 +4,17 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+# database_url =
+# System.get_env("DATABASE_URL") ||
+# raise """
+# environment variable DATABASE_URL is missing.
+# For example: ecto://USER:PASS@HOST/DATABASE
+# """
 
-config :pay, Pay.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+# config :pay, Pay.Repo,
+# # ssl: true,
+# url: database_url,
+# pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
@@ -24,15 +24,15 @@ secret_key_base =
     """
 
 config :pay, PayWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [port: {:system, "PORT"}],
   secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
-#
-#     config :pay, PayWeb.Endpoint, server: true
-#
+
+config :pay, PayWeb.Endpoint, server: true
+
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
--- lib/pay/repo.ex
+++ lib/pay/repo.ex
@@ -2,4 +2,16 @@ defmodule Pay.Repo do
   use Ecto.Repo,
     otp_app: :pay,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_type, config) do
+    with {:ok, services} <- Pay.VCAP.services() do
+      postgres = services["postgres"]
+      database = Enum.at(postgres, 0)
+      database_uri = database["credentials"]["uri"]
+      {:ok, Keyword.put(config, :url, database_uri)}
+    else
+      _ ->
+        {:ok, config}
+    end
+  end
 end
