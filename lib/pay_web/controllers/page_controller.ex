31d1b2fbbcfff4b94393f3c29c883a85b4eb348f
Peter DeGroff <peter.degroff@gmail.com>
2019-09-19 12:06:28 +1000

    sentry: add sentry and erroring controller

--- lib/pay_web/controllers/page_controller.ex
+++ lib/pay_web/controllers/page_controller.ex
@@ -4,4 +4,12 @@ defmodule PayWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def status(conn, %{"err" => "true"}) do
+    json(conn, %{status: 5 = 3})
+  end
+
+  def status(conn, _params) do
+    json(conn, %{status: "OK"})
+  end
 end
