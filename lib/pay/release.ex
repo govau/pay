e3b8292e8721c3cba60773d1f0d08305053b41f3
Peter DeGroff <peter.degroff@gmail.com>
2019-09-20 13:48:19 +1000

    migrations: add script to migrate in prod release

---
+++ lib/pay/release.ex
@@ -0,0 +1,24 @@
+defmodule Pay.Release do
+  @app :pay
+
+  def migrate do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  def repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(@app)
+  end
+end
