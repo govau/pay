6990c82f52618671202af7be2b1e78b6c107234d
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2019-09-25 11:04:59 +1000

    public assets and things

--- assets/src/index.tsx
+++ assets/src/index.tsx
@@ -1,7 +1,15 @@
 import "core-js/features/array/find";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import * as Sentry from "@sentry/browser";
+
 import App from "./App";
+
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    environment: process.env.REACT_APP_SENTRY_ENV || ""
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
