5e4912836263d28df95e6826a04b1602204d3ad7
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2019-11-06 13:25:51 +1100

    credit card input attributes

--- assets/src/pay/components/SandboxCardForm.tsx
+++ assets/src/pay/components/SandboxCardForm.tsx
@@ -19,7 +19,16 @@ const SandboxCardForm: React.FC<Props> = ({ children, onSubmit }) => (
   <Form onSubmit={onSubmit} column>
     <Field name="cardNumber" label="Card number">
       {({ input, ariaProps, ...rest }) => (
-        <BasicTextInput {...input} {...ariaProps} {...rest} />
+        <BasicTextInput
+          {...input}
+          {...ariaProps}
+          {...rest}
+          inputMode="numeric"
+          autoComplete="cc-number"
+          autoCorrect="off"
+          spellCheck={false}
+          pattern="[\d| ]{16,22}"
+        />
       )}
     </Field>
 
@@ -32,7 +41,16 @@ const SandboxCardForm: React.FC<Props> = ({ children, onSubmit }) => (
     >
       {({ input, ariaProps, ...rest }) => (
         <ExpiryInputWrapper>
-          <BasicTextInput {...input} {...ariaProps} {...rest} />
+          <BasicTextInput
+            {...input}
+            {...ariaProps}
+            {...rest}
+            inputMode="numeric"
+            autoComplete="cc-exp"
+            autoCorrect="off"
+            spellCheck={false}
+            pattern="\d\d/\d\d"
+          />
         </ExpiryInputWrapper>
       )}
     </Field>
@@ -44,7 +62,16 @@ const SandboxCardForm: React.FC<Props> = ({ children, onSubmit }) => (
     >
       {({ input, ariaProps, ...rest }) => (
         <CVVInputWrapper>
-          <BasicTextInput {...input} {...ariaProps} {...rest} />
+          <BasicTextInput
+            {...input}
+            {...ariaProps}
+            {...rest}
+            inputMode="numeric"
+            autoComplete="cc-csc"
+            autoCorrect="off"
+            spellCheck={false}
+            pattern="\d{3,4}"
+          />
         </CVVInputWrapper>
       )}
     </Field>
@@ -55,7 +82,14 @@ const SandboxCardForm: React.FC<Props> = ({ children, onSubmit }) => (
       description="We’ll send your payment confirmation here"
     >
       {({ input, ariaProps, ...rest }) => (
-        <BasicTextInput {...input} {...ariaProps} {...rest} type="email" />
+        <BasicTextInput
+          {...input}
+          {...ariaProps}
+          {...rest}
+          type="email"
+          autoCorrect="off"
+          autoComplete="email"
+        />
       )}
     </Field>
 
