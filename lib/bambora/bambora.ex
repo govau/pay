eed681a67653faf6277845b5cbf1208defb65094
Peter DeGroff <peter.degroff@digital.gov.au>
2019-11-11 17:19:25 +1100

    bambora: add refund and implement gateway protocol

--- lib/bambora/bambora.ex
+++ lib/bambora/bambora.ex
@@ -20,6 +20,21 @@ defmodule Bambora do
     end
   end
 
+  @spec submit_single_refund(
+          Bambora.Client.t(),
+          Bambora.Service.SubmitSingleRefund.params()
+        ) :: {:ok, Bambora.Service.SubmitSingleRefund.response()} | {:error, String.t()}
+  def submit_single_refund(client, params) do
+    with {:ok, %{response: response}} <-
+           Bambora.Client.make_request(
+             client,
+             Service.SubmitSingleRefund,
+             params
+           ) do
+      {:ok, response}
+    end
+  end
+
   @spec query_todays_payments(Bambora.Client.t()) :: {:ok, map} | {:error, String.t()}
   def query_todays_payments(client) do
     Bambora.Client.make_request(client, Service.QueryTransaction, %{
