3528458c36a927e12077432dd83d5e5d01a86c1a
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2019-11-22 16:55:20 +1100

    ui: allow faking signed in user for now

--- lib/pay_web/plugs/set_current_user.ex
+++ lib/pay_web/plugs/set_current_user.ex
@@ -2,7 +2,6 @@ defmodule PayWeb.Plugs.SetCurrentUser do
   import Plug.Conn
 
   alias Pay.Repo
-  alias Pay.Services
   alias Pay.Services.User
 
   def init(_params) do
@@ -12,14 +11,12 @@ defmodule PayWeb.Plugs.SetCurrentUser do
     if conn.assigns[:current_user] do
       conn
     else
-      # TODO: get user from session or wherever and tmp_user.id becomes user_id
+      # TODO: get user from session or wherever
+      user_id = get_req_header(conn, "authorization") |> List.first()
       # user_id = Plug.Conn.get_session(conn, :current_user_id)
-      users = Services.list_users()
-      # tmp_user = Enum.at(users, 4)
-      tmp_user = users |> Enum.filter(fn u -> u.platform_admin == true end) |> List.first()
 
       cond do
-        current_user = tmp_user && tmp_user.id && Repo.get(User, tmp_user.id) ->
+        current_user = user_id != nil && user_id != "" && Repo.get_by(User, external_id: user_id) ->
           conn
           |> assign(:current_user, current_user)
 
