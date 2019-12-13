3c86da92752c055c160489b3556d37a83cc77682
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2019-12-13 14:37:00 +1100

    products: don't send bad token to pay API
    Sending TODO will cause the Pay API to think that there is a valid token
    to decode and authenticate. Instead we don't pass anything until we
    implement it.

--- lib/pay_web/controllers/products/pay_client.ex
+++ lib/pay_web/controllers/products/pay_client.ex
@@ -1,14 +1,14 @@
 defmodule PayWeb.Products.PayClient do
   # TODO: pass in auth.
   def new() do
-    token = "TODO"
-
     middleware = [
       {Tesla.Middleware.Timeout, timeout: 5_000},
       {Tesla.Middleware.BaseUrl, PayWeb.Endpoint.url()},
       {Tesla.Middleware.EncodeJson, engine: Poison},
       {Tesla.Middleware.Headers,
-       [{"authorization", "token: " <> token}, {"user-agent", "Elixir"}]}
+       [
+         {"user-agent", "Elixir"}
+       ]}
     ]
 
     Tesla.client(middleware)
