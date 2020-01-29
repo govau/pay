c5c15a75a75448dd280b689dd66af7792ce59ef6
Peter DeGroff <peter.degroff@digital.gov.au>
2020-01-29 16:16:05 +1100

    frontend: update service settings page design

--- assets/src/console/pages/services/SettingsPage.tsx
+++ assets/src/console/pages/services/SettingsPage.tsx
@@ -1,9 +1,21 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
-import { TODO, PageTitle, Link } from "@pay/web";
+import { PageTitle, Link, P, styled } from "@pay/web";
 
 import { GatewayAccountType } from "../../__generated__/graphql";
 import { BreadBox } from "@pay/web/components/Breadcrumb";
+
+const Ul = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const Li = styled.li`
+  & ${Link} + ${P} {
+    margin-top: 0;
+  }
+`;
 
 export interface props {
   service: { externalId: string; name: string };
@@ -29,42 +41,53 @@ const SettingsPage: React.FC<props> = ({ service, gatewayAccounts }) => {
         <title>Settings - {service.name}</title>
       </Helmet>
       <PageTitle
-        title="Settings"
+        title="Service settings"
         breadcrumbs={BreadBox.Settings({ service })}
       />
-      <TODO>
-        <ul>
-          <li>
-            <Link to={`/console/services/${service.externalId}/edit-name`}>
-              Edit name
-            </Link>{" "}
-          </li>
-          <li>
-            <Link to={`/console/services/${service.externalId}/team`}>
-              Manage service team members
-            </Link>
-          </li>
-          <li>Manage API keys</li>
-          {gatewayAccount && (
-            <li>
+      <Ul>
+        <Li>
+          <Link to={`/console/services/${service.externalId}/edit-name`}>
+            Edit name
+          </Link>
+          <P>
+            Change the name of this service as it appears on payment pages and
+            for administrators.
+          </P>
+        </Li>
+
+        <Li>
+          <Link to={`/console/services/${service.externalId}/team`}>
+            Manage team members
+          </Link>
+          <P>
+            Invite new team members or change the role of an existing member.
+          </P>
+        </Li>
+
+        {gatewayAccount && (
+          <>
+            <Li>
               <Link
                 to={`/console/services/${service.externalId}/gateway-accounts/${gatewayAccount.externalId}/credentials`}
               >
-                Manage payment service provider account credentials
+                Configure payment provider
               </Link>
-            </li>
-          )}
-          {gatewayAccount && (
-            <li>
+              <P>
+                Set up a payment provider to process payments for your service.
+              </P>
+            </Li>
+
+            <Li>
               <Link
                 to={`/console/services/${service.externalId}/gateway-accounts/${gatewayAccount.externalId}/card-types`}
               >
-                Manage the cards that you accept
+                Manage payment types
               </Link>
-            </li>
-          )}
-        </ul>
-      </TODO>
+              <P>Choose which credit and debit cards you want to accept.</P>
+            </Li>
+          </>
+        )}
+      </Ul>
     </>
   );
 };
