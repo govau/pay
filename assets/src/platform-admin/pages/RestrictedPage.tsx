ab19a6edefec39a10576669b24d4c4d19b4a1706
Anitha Manickam <ani.manickam@gmail.com>
2020-03-02 11:43:03 +1100

    frontend: fix for flickering authentication error message

--- assets/src/platform-admin/pages/RestrictedPage.tsx
+++ assets/src/platform-admin/pages/RestrictedPage.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
-import { ErrorAlert } from "@pay/web";
-
-import { usePayUser } from "../../users";
+import { ErrorAlert, Loader } from "@pay/web";
+import { useAuth0 } from "../../auth/AuthContext";
 
 interface Props {
   children: JSX.Element;
 }
 
 const RestrictedPage: React.FC<Props> = ({ children }) => {
-  const user = usePayUser();
+  const { isInitializing, isAuthenticated } = useAuth0();
 
-  if (user && user.platformAdmin) {
+  if (isInitializing) return <Loader message="Loading " />;
+
+  if (isAuthenticated) {
     return children;
   }
 
