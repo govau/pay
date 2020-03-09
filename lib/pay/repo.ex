230bb693ea933a7a727f4d05dfaa7c5f5728c85e
Peter DeGroff <peter.degroff@digital.gov.au>
2020-03-09 14:35:37 +1100

    environments: use new db in prod, target new space

--- lib/pay/repo.ex
+++ lib/pay/repo.ex
@@ -3,15 +3,36 @@ defmodule Pay.Repo do
     otp_app: :pay,
     adapter: Ecto.Adapters.Postgres
 
+  def postgres_config(config, %{"postgres" => [database]}) do
+    %{"uri" => database_uri} = database["credentials"]
+    {:ok, Keyword.put(config, :url, database_uri)}
+  end
+
+  def postgres_config(config, %{"postgresql" => [database]}) do
+    %{
+      "DB_NAME" => database_name,
+      "MASTER_USERNAME" => username,
+      "MASTER_PASSWORD" => password,
+      "ENDPOINT_ADDRESS" => hostname,
+      "PORT" => port
+    } = database["credentials"]
+
+    config =
+      config
+      |> Keyword.put(:database, database_name)
+      |> Keyword.put(:username, username)
+      |> Keyword.put(:password, password)
+      |> Keyword.put(:hostname, hostname)
+      |> Keyword.put(:port, port)
+
+    {:ok, config}
+  end
+
   def init(_type, config) do
     with {:ok, services} <- Pay.VCAP.services() do
-      postgres = services["postgres"]
-      database = Enum.at(postgres, 0)
-      database_uri = database["credentials"]["uri"]
-      {:ok, Keyword.put(config, :url, database_uri)}
+      postgres_config(config, services)
     else
-      _ ->
-        {:ok, config}
+      _ -> {:ok, config}
     end
   end
 end
